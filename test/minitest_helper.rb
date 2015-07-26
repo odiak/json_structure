9872e35329588cf1e533232f477fc03b4cd82ab3
Kaido Iwamoto <h6824.kaido@gmail.com>
2015-07-27 00:40:15 +0900

    Setup for test

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,3 +2,14 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'json_structure'
 
 require 'minitest/autorun'
+require 'minitest/spec'
+
+module JSHelper
+  def build(&block)
+    JsonStructure.build(&block)
+  end
+end
+
+class Minitest::Spec
+  include JSHelper
+end
